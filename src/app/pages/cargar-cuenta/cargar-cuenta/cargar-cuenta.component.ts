67138b9acdc7b9f614e90b4fe44da6309c84a588
Hugo Sanchez de la Vega <hsnachez@aspintegraopciones.com>
2023-07-11 18:59:23 -0700

    Diseño de tabla y carga de archivo xlsx

--- src/app/pages/cargar-cuenta/cargar-cuenta/cargar-cuenta.component.ts
+++ src/app/pages/cargar-cuenta/cargar-cuenta/cargar-cuenta.component.ts
@@ -6,28 +6,43 @@ import { InfoPersonaFisica } from 'src/app/_model/InfoPersonaFisica';
 import { LocalStorageService } from 'src/app/_service/local-storage.service';
 import * as XLSX from 'xlsx';
 
-
-
 @Component({
   selector: 'app-cargar-cuenta',
   templateUrl: './cargar-cuenta.component.html',
-  styleUrls: ['./cargar-cuenta.component.css']
+  styleUrls: ['./cargar-cuenta.component.css'],
 })
 export class CargarCuentaComponent implements OnInit {
-  datosExcel: InfoPersonaFisica[] = []
-  datos: any[] = []//datos que trae el excel
-  divEscondido: boolean = true;//la variable del div que contiene la tabla de los datos del excel
-  displayedColumns: string[] = ['Destino', 'Beneficiario', 'Número de Cuenta', 'Banco', 'Monto', 'Ref. Num', 'Concepto Pago'];
+  datosExcel: InfoPersonaFisica[] = [];
+  datos: any[] = []; //datos que trae el excel
+  divEscondido: boolean = true; //la variable del div que contiene la tabla de los datos del excel
+  displayedColumns: string[] = [
+    'Correo',
+    'Teléfono',
+    'Nombre',
+    'ID Ocupación',
+    'Sexo',
+    'Entidad de Nacimiento',
+    'Apellido Paterno',
+    'Apellido Materno',
+    'Num. Identificación O.F.',
+    'RFC',
+    'CURP',
+    'Calle Principal',
+    'Num. Exterior',
+    'Num. Interior',
+    'Colonia',
+    'Código Postal',
+    'Fecha de Nacimiento'
+  ];
   dataSource!: MatTableDataSource<InfoPersonaFisica>;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-
-  constructor(private localStorageService: LocalStorageService) { }
+  constructor(private localStorageService: LocalStorageService) {}
 
   ngOnInit(): void {
-    if (this.localStorageService.getExcel("datosExcel") != null) {
-      this.datosExcel = this.localStorageService.getExcel("datosExcel");
+    if (this.localStorageService.getExcel('datosExcel') != null) {
+      this.datosExcel = this.localStorageService.getExcel('datosExcel');
       this.dataSource = new MatTableDataSource(this.datosExcel);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
@@ -41,45 +56,57 @@ export class CargarCuentaComponent implements OnInit {
     this.dataSource = new MatTableDataSource(this.datosExcel);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-    console.log(this.datosExcel)
+    console.log(this.datosExcel);
     /*for (let m of this.datosExcel) {
       this.montoTotal = this.montoTotal + parseFloat(m.monto);
     }*/
   }
-  cargarXLSX(event: Event){
+  cargarXLSX(event: Event) {
     const inputElement = event.target as HTMLInputElement;
     let archivo = inputElement.files;
-    if(archivo != null){
-      this.datosExcel = []
+    if (archivo != null) {
+      this.datosExcel = [];
       const lector = new FileReader();
       lector.readAsBinaryString(archivo[0]);
       lector.onload = () => {
         const libro = XLSX.read(lector.result, { type: 'binary' });
         const hoja = libro.Sheets[libro.SheetNames[0]];
         this.datos = XLSX.utils.sheet_to_json(hoja);
-        for (let i = 0; i < this.datos.length; i++) {
-          if (this.datos[i]['Destino'].length == 18) {
-            let trans = new InfoPersonaFisica();
-            trans.ctaDestino = this.datos[i]['Destino'];
-            trans.nombreDestino = this.datos[i]['Nombre Beneficiario'];
-            trans.clabe = this.datos[i]['Numero de cuenta'];
-            trans.bancoDestino = this.datos[i]['Institucion bancaria'];
-            trans.monto = this.datos[i]['Monto'];
-            trans.refNum = this.datos[i]['Referencia Numerica'];
-            trans.conceptoPago = this.datos[i]['Concepto pago'];
-            trans.cveRastreo = "asw2";
-            this.datosExcel.push(trans)
+        for (const element of this.datos) {
+          if (
+            element['correo'] &&
+            element['telefono'] &&
+            element['idOcupacion'] &&
+            element['celular'] &&
+            element['entidadNacimiento'] &&
+            element['numIdentificacionOf']
+          ) {
+            let dataPersona = new InfoPersonaFisica();
+            dataPersona.correo = element['correo'];
+            dataPersona.telefono = element['telefono'];
+            dataPersona.nombre = element['nombre'];
+            dataPersona.idOcupacion = element['idOcupacion'];
+            dataPersona.sexo = element['sexo'];
+            dataPersona.entidadNacimiento = element['entidadNacimiento'];
+            dataPersona.apellidoPaterno = element['apellidoPaterno'];
+            dataPersona.apellidoMaterno = element['apellidoMaterno'];
+            dataPersona.numIdentificacionOf = element['numIdentificacionOf'];
+            dataPersona.rfc = element['rfc'];
+            dataPersona.curp = element['curp'];
+            dataPersona.callePrincipal = element['callePrincipal'];
+            dataPersona.numExterior = element['numExterior'];
+            dataPersona.numInterior = element['numInterior'];
+            dataPersona.colonia = element['colonia'];
+            dataPersona.codPostal = element['codPostal'];
+            dataPersona.fechaNacimiento = element['fechaNacimiento'];
+            this.datosExcel.push(dataPersona);
           }
-        };
-        this.localStorageService.setExcel("datosExcel", this.datosExcel);
+        }
+        this.localStorageService.setExcel('datosExcel', this.datosExcel);
         this.divEscondido = false;
         this.ngAfterViewInit();
       };
     }
-  }
-
-  applyFilter($event: Event){
-    //TODO
   }
 
 }
